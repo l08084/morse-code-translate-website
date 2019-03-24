452ded2dc5b90d8612033cf565e4bb91189b7fa4
l08084 <l08084.1989@gmail.com>
2019-03-24 16:38:23 +0900

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { InputComponent } from './components/input/input.component';
+import { MatInputModule } from '@angular/material/input';
 import { OutputComponent } from './components/output/output.component';
 
 @NgModule({
   declarations: [AppComponent, InputComponent, OutputComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
