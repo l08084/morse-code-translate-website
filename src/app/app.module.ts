5ac23ca93efacae20174ba4b08cc951b0cc8d121
l08084 <l08084.1989@gmail.com>
2019-03-25 21:43:53 +0900

    add reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { AppComponent } from './app.component';
 import { InputComponent } from './components/input/input.component';
 import { MatInputModule } from '@angular/material/input';
 import { OutputComponent } from './components/output/output.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [AppComponent, InputComponent, OutputComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatInputModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent {}
+export class InputComponent implements OnInit {
+  public inputForm: FormGroup;
+  public japaneseControl: FormControl;
+
+  constructor(private builder: FormBuilder) {
+    // 入力フォームの初期設定を行う
+    this.createForm();
+  }
+
+  public ngOnInit(): void {
+    this.japaneseControl = this.inputForm.get('japanese') as FormControl;
+  }
+
+  private createForm() {
+    this.inputForm = this.builder.group({
+      japanese: ['', []]
+    });
+  }
+}
